8c4a3b2bb80272aecab18982a2d854a7a39905a8
John T. Wodder II <git@varonathe.org>
2018-05-31 23:02:45 +0000

    Made tests pass

--- TODO.md
+++ TODO.md
@@ -1,4 +1,8 @@
+- "`tabulator`" is taken.  Come up with a new name.
+- Fill in keywords, classifiers, & package description
 - Support Python 3
+- Stringifying a Tabulator object should return the typeset table (no trailing
+  newline)
 
 Features to Add
 ---------------
@@ -24,6 +28,8 @@ Features to Add
 - Parameter for adding vertical but not horizontal borders
 - Parameter for adding horizontal but not vertical borders
 - Centering & right-aligning of specified columns
+- Setting the text alignment of individual cells (e.g., having a centered "—"
+  cell in the middle of a bunch of left-aligned cells)
 - Add a method that simply returns the padded & folded cells as an array of
   arrays of strings rather than formatting them
 - cells that span multiple columns and/or rows
@@ -35,8 +41,11 @@ Features to Add
   ('dash', 'joint', '`left_joint`', '`right_joint`', 'midjoint'?)
 - parameter for setting how to display `None`s
 - parameter for setting how to show bools?
-- parameter for producing Markdown- or reStructuredText-compatible tables
-- parameter for producing HTML tables
+- Producing Markdown- or reStructuredText-compatible tables
+- Producing HTML tables
+- Producing PDF tables with reportlab?
 - parsing a text table back into a Tabulator object?
 - transposition
-- handling of Unicode characters with non-unit width
+- handling of Unicode characters with non-unit width (cf. the `wcwidth`
+  package)
+- forcing all columns to have the same width?
--- tabulator.py
+++ tabulator.py
@@ -1,30 +1,44 @@
-from itertools import izip_longest
+__version__      = '0.1.0.dev1'
+__author__       = 'John Thorvald Wodder II'
+__author_email__ = 'tabulator@varonathe.org'
+__license__      = 'MIT'
+__url__          = 'https://github.com/jwodder/tabulator'
 
-__all__ = ["Tabulator"]
+from six.moves import zip_longest
 
 class Tabulator(object):
-    def __init__(self, include_extra_columns=True,
-                       extra_header=u'',
-                       fill_empty_columns=u'',
-                       border=False,
+    def __init__(self, data=(),
+                       headers=None,
+                       include_extra_columns=True,
+                       extra_header='',
+                       fill_empty_columns='',
+                       border=True,
                        trim_spaces=True):
+        self.data = list(map(list, data))
+        self.headers = list(headers) if headers is not None else None
         self.include_extra_columns = include_extra_columns
         self.extra_header = extra_header
         self.fill_empty_columns = fill_empty_columns
         self.border = border
         self.trim_spaces = trim_spaces
 
-    def show(self, table, headers=None):
+    def append(self, row):
+        self.data.append(list(row))
+
+    def extend(self, data):
+        self.data.extend(map(list, data))
+
+    def __str__(self):
         data = []
-        if headers is not None:
-            headers = map(unistr, headers)
+        if self.headers is not None:
+            headers = list(map(str, self.headers))
             columns = len(headers)
-            widths = [max(len, h.splitlines()) for h in headers]
+            widths = [max(map(len, h.splitlines())) for h in headers]
         else:
             columns = 0
             widths = []
-        for row in table:
-            row = map(unistr, row)
+        for row in self.data:
+            row = list(map(str, row))
             if len(row) > columns:
                 if headers is not None and not self.include_extra_columns:
                     row = row[:columns]
@@ -32,45 +46,32 @@ class Tabulator(object):
                     columns = max(len(row), columns)
             elif len(row) < columns:
                 row = (row + [self.fill_empty_columns] * columns)[:columns]
-            widths = map(max, izip_longest(widths, map(len, row), fillvalue=0))
+            widths = map(max, zip_longest(widths, map(len, row), fillvalue=0))
             data.append(row)
+        widths = list(widths)
+
         def showrow(row):
-            s = u''
-            for row_ in izip_longest(*map(unicode.splitlines, row),
-                                     fillvalue=u''):
-                s1 = u'|'.join(u'%-*s' % (w, cell)
-                               for (w, cell) in zip(widths, row))
+            s = ''
+            for r in zip_longest(*map(str.splitlines, row), fillvalue=''):
+                s1 = '|'.join('%-*s' % (w, cell)
+                              for (w, cell) in zip(widths, r))
                 if self.border:
-                    s1 = u'|' + s1 + u'|'
+                    s1 = '|' + s1 + '|'
                 elif self.trim_spaces:
                     s1 = s1.rstrip()
-                s += s1 + u'\n'
+                s += s1
             return s
-        hrule = u'+'.join(u'-' * w for w in widths)
-        if self.border:
-            hrule = u'+' + hrule + u'+'
-        hrule += u'\n'
-        output = u''
-        if self.border:
-            output += hrule
-        if self.headers is not None:
-            output += showrow(headers + [self.extra_header] * (columns - len(headers)))
-            output += hrule
-        output += u''.join(showrow(row) for row in data)
-        if self.border:
-            output += hrule
-        return output
 
-def unistr(val):  # internal helper function
-    try:
-        s = unicode(val)
-    except Exception:
-        try:
-            s1 = str(val)
-        except Exception:
-            s1 = repr(val)
-        try:
-            s = s1.decode('utf-8')
-        except UnicodeDecodeError:
-            s = s1.decode('latin-1')
-    return s
+        hrule = '+'.join('-' * w for w in widths)
+        if self.border:
+            hrule = '+' + hrule + '+'
+        output = ''
+        if self.border:
+            output += hrule + '\n'
+        if self.headers is not None:
+            output += showrow(headers + [self.extra_header] * (columns - len(headers))) + '\n'
+            output += hrule + '\n'
+        output += '\n'.join(map(showrow, data))
+        if self.border:
+            output += '\n' + hrule
+        return output
