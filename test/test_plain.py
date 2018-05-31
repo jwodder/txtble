87b5f6246b10a26f6edbb1da89b8ce84264d4c2e
John T. Wodder II <git@varonathe.org>
2018-05-31 21:49:14 +0000

    First design-driving tests for new API

---
+++ test/test_plain.py
@@ -0,0 +1,51 @@
+from tabulator import Tabulator
+
+# Taken from /usr/share/misc/birthtoken.gz in Ubuntu Xenial's miscfiles package:
+HEADERS = ['Month', 'Birthstone', 'Birth Flower']
+DATA = [
+    ['January',   'Garnet',     'Carnation'],
+    ['February',  'Amethyst',   'Violet'],
+    ['March',     'Aquamarine', 'Jonquil'],
+    ['April',     'Diamond',    'Sweetpea'],
+    ['May',       'Emerald',    'Lily Of The Valley'],
+    ['June',      'Pearl',      'Rose'],
+    ['July',      'Ruby',       'Larkspur'],
+    ['August',    'Peridot',    'Gladiolus'],
+    ['September', 'Sapphire',   'Aster'],
+    ['October',   'Opal',       'Calendula'],
+    ['November',  'Topaz',      'Chrysanthemum'],
+    ['December',  'Turquoise',  'Narcissus'],
+]
+
+TABLE = '''\
++---------+----------+------------------+
+|Month    |Birthstone|Birth Flower      |
++---------+----------+------------------+
+|January  |Garnet    |Carnation         |
+|February |Amethyst  |Violet            |
+|March    |Aquamarine|Jonquil           |
+|April    |Diamond   |Sweetpea          |
+|May      |Emerald   |Lily Of The Valley|
+|June     |Pearl     |Rose              |
+|July     |Ruby      |Larkspur          |
+|August   |Peridot   |Gladiolus         |
+|September|Sapphire  |Aster             |
+|October  |Opal      |Calendula         |
+|November |Topaz     |Chrysanthemum     |
+|December |Turquoise |Narcissus         |
++---------+----------+------------------+'''
+
+def test_one_expression():
+    tbl = Tabulator(DATA, headers=HEADERS)
+    assert str(tbl) == TABLE
+
+def test_append_each():
+    tbl = Tabulator(headers=HEADERS)
+    for row in DATA:
+        tbl.append(row)
+    assert str(tbl) == TABLE
+
+def test_extend_all():
+    tbl = Tabulator(headers=HEADERS)
+    tbl.extend(DATA)
+    assert str(tbl) == TABLE
