6cb1439d84b295b69b724606ab2c7efa64e05b0d
John T. Wodder II <git@varonathe.org>
2018-06-05 12:19:49 -0400

    Test that non-strings are stringified with str()

--- test/test_nonstr.py
+++ test/test_nonstr.py
@@ -1,4 +1,17 @@
+# -*- coding: utf-8 -*-
+from six       import text_type
 from tabulator import Tabulator
+
+class Custom(object):
+    def __str__(self):
+        return 'str'
+
+    def __unicode__(self):
+        return u'unicode'
+
+    def __bytes__(self):
+        return b'bytes'
+
 
 def test_none():
     tbl = Tabulator(
@@ -37,4 +50,28 @@ def test_none_str_empty():
         "|None  |None |\n"
         "|'None'|None |\n"
         '+------+-----+'
+    )
+
+def test_custom():
+    tbl = Tabulator(data=[['A', 'B'], ['C', Custom()]])
+    assert str(tbl) == (
+        '+-+---+\n'
+        '|A|B  |\n'
+        '|C|str|\n'
+        '+-+---+'
+    )
+    assert text_type(tbl) == (
+        u'+-+---+\n'
+        u'|A|B  |\n'
+        u'|C|str|\n'
+        u'+-+---+'
+    )
+
+def test_custom_plus_unicode():
+    tbl = Tabulator(data=[[u'Å', u'B'], [u'Č', Custom()]])
+    assert text_type(tbl) == (
+        u'+-+---+\n'
+        u'|Å|B  |\n'
+        u'|Č|str|\n'
+        u'+-+---+'
     )
