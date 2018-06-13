8d44170125980707ecf1840ab305e8b401816302
John T. Wodder II <git@varonathe.org>
2018-06-12 21:00:04 -0400

    More border tests

--- test/test_plain.py
+++ test/test_plain.py
@@ -391,6 +391,92 @@ def test_row_border_no_header_border():
         '+---------+----------+------------------+'
     )
 
+def test_all_borders_off():
+    tbl = Txtble(
+        DATA,
+        border        = False,
+        column_border = False,
+        header_border = False,
+        headers       = HEADERS,
+    )
+    assert str(tbl) == (
+        'Month    BirthstoneBirth Flower\n'
+        'January  Garnet    Carnation\n'
+        'February Amethyst  Violet\n'
+        'March    AquamarineJonquil\n'
+        'April    Diamond   Sweetpea\n'
+        'May      Emerald   Lily Of The Valley\n'
+        'June     Pearl     Rose\n'
+        'July     Ruby      Larkspur\n'
+        'August   Peridot   Gladiolus\n'
+        'SeptemberSapphire  Aster\n'
+        'October  Opal      Calendula\n'
+        'November Topaz     Chrysanthemum\n'
+        'December Turquoise Narcissus'
+    )
+
+def test_invert_all_borders():
+    tbl = Txtble(
+        DATA,
+        border        = False,
+        column_border = False,
+        header_border = False,
+        headers       = HEADERS,
+        row_border    = True,
+    )
+    assert str(tbl) == (
+        'Month    BirthstoneBirth Flower\n'
+        'January  Garnet    Carnation\n'
+        '-------------------------------------\n'
+        'February Amethyst  Violet\n'
+        '-------------------------------------\n'
+        'March    AquamarineJonquil\n'
+        '-------------------------------------\n'
+        'April    Diamond   Sweetpea\n'
+        '-------------------------------------\n'
+        'May      Emerald   Lily Of The Valley\n'
+        '-------------------------------------\n'
+        'June     Pearl     Rose\n'
+        '-------------------------------------\n'
+        'July     Ruby      Larkspur\n'
+        '-------------------------------------\n'
+        'August   Peridot   Gladiolus\n'
+        '-------------------------------------\n'
+        'SeptemberSapphire  Aster\n'
+        '-------------------------------------\n'
+        'October  Opal      Calendula\n'
+        '-------------------------------------\n'
+        'November Topaz     Chrysanthemum\n'
+        '-------------------------------------\n'
+        'December Turquoise Narcissus'
+    )
+
+def test_outer_border_only():
+    tbl = Txtble(
+        DATA,
+        border        = True,
+        column_border = False,
+        header_border = False,
+        headers       = HEADERS,
+    )
+    assert str(tbl) == (
+        '+-------------------------------------+\n'
+        '|Month    BirthstoneBirth Flower      |\n'
+        '|January  Garnet    Carnation         |\n'
+        '|February Amethyst  Violet            |\n'
+        '|March    AquamarineJonquil           |\n'
+        '|April    Diamond   Sweetpea          |\n'
+        '|May      Emerald   Lily Of The Valley|\n'
+        '|June     Pearl     Rose              |\n'
+        '|July     Ruby      Larkspur          |\n'
+        '|August   Peridot   Gladiolus         |\n'
+        '|SeptemberSapphire  Aster             |\n'
+        '|October  Opal      Calendula         |\n'
+        '|November Topaz     Chrysanthemum     |\n'
+        '|December Turquoise Narcissus         |\n'
+        '+-------------------------------------+'
+    )
+
 def test_tabs():
     tbl = Txtble(
         headers=['Head\ter'],
