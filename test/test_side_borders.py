de1a71a97ad8919baea211629e41b72c2c33b6e2
John T. Wodder II <git@varonathe.org>
2018-07-19 19:05:01 -0400

    Test side border styles

--- test/test_side_borders.py
+++ test/test_side_borders.py
@@ -1,5 +1,11 @@
-from txtble    import Txtble
-from test_data import HEADERS, DATA
+# -*- coding: utf-8 -*-
+import pytest
+from   six       import text_type
+from   txtble    import (
+    Txtble,
+    ASCII_EQ_BORDERS, LIGHT_BORDERS, HEAVY_BORDERS, DOUBLE_BORDERS, DOT_BORDERS,
+)
+from   test_data import HEADERS, DATA
 
 def test_vborders_off():
     tbl = Txtble(DATA, headers=HEADERS, left_border=False, right_border=False)
@@ -20,6 +26,33 @@ def test_vborders_off():
         'November |Topaz     |Chrysanthemum\n'
         'December |Turquoise |Narcissus\n'
         '---------+----------+------------------'
+    )
+
+def test_vborders_off_styled():
+    tbl = Txtble(
+        DATA,
+        headers      = HEADERS,
+        border       = ASCII_EQ_BORDERS,
+        left_border  = False,
+        right_border = False,
+    )
+    assert str(tbl) == (
+        '=========+==========+==================\n'
+        'Month    |Birthstone|Birth Flower\n'
+        '---------+----------+------------------\n'
+        'January  |Garnet    |Carnation\n'
+        'February |Amethyst  |Violet\n'
+        'March    |Aquamarine|Jonquil\n'
+        'April    |Diamond   |Sweetpea\n'
+        'May      |Emerald   |Lily Of The Valley\n'
+        'June     |Pearl     |Rose\n'
+        'July     |Ruby      |Larkspur\n'
+        'August   |Peridot   |Gladiolus\n'
+        'September|Sapphire  |Aster\n'
+        'October  |Opal      |Calendula\n'
+        'November |Topaz     |Chrysanthemum\n'
+        'December |Turquoise |Narcissus\n'
+        '=========+==========+=================='
     )
 
 def test_hborders_on():
@@ -47,6 +80,34 @@ def test_hborders_on():
         'November |Topaz     |Chrysanthemum\n'
         'December |Turquoise |Narcissus\n'
         '---------+----------+------------------'
+    )
+
+def test_hborders_on_styled():
+    tbl = Txtble(
+        DATA,
+        headers       = HEADERS,
+        border        = False,
+        border_style  = ASCII_EQ_BORDERS,
+        top_border    = True,
+        bottom_border = True,
+    )
+    assert str(tbl) == (
+        '=========+==========+==================\n'
+        'Month    |Birthstone|Birth Flower\n'
+        '=========+==========+==================\n'
+        'January  |Garnet    |Carnation\n'
+        'February |Amethyst  |Violet\n'
+        'March    |Aquamarine|Jonquil\n'
+        'April    |Diamond   |Sweetpea\n'
+        'May      |Emerald   |Lily Of The Valley\n'
+        'June     |Pearl     |Rose\n'
+        'July     |Ruby      |Larkspur\n'
+        'August   |Peridot   |Gladiolus\n'
+        'September|Sapphire  |Aster\n'
+        'October  |Opal      |Calendula\n'
+        'November |Topaz     |Chrysanthemum\n'
+        'December |Turquoise |Narcissus\n'
+        '=========+==========+=================='
     )
 
 def test_hborders_off():
@@ -148,4 +209,34 @@ def test_bottom_border_only():
         'November |Topaz     |Chrysanthemum\n'
         'December |Turquoise |Narcissus\n'
         '---------+----------+------------------'
+    )
+
+@pytest.mark.parametrize('border', [True, False, DOT_BORDERS])
+def test_every_side_different_style(border):
+    tbl = Txtble(
+        DATA,
+        headers       = HEADERS,
+        border        = border,
+        top_border    = HEAVY_BORDERS,
+        bottom_border = ASCII_EQ_BORDERS,
+        left_border   = LIGHT_BORDERS,
+        right_border  = DOUBLE_BORDERS,
+    )
+    assert text_type(tbl) == (
+        u'┏━━━━━━━━━┳━━━━━━━━━━┳━━━━━━━━━━━━━━━━━━┓\n'
+        u'│Month    |Birthstone|Birth Flower      ║\n'
+        u'+---------+----------+------------------+\n'
+        u'│January  |Garnet    |Carnation         ║\n'
+        u'│February |Amethyst  |Violet            ║\n'
+        u'│March    |Aquamarine|Jonquil           ║\n'
+        u'│April    |Diamond   |Sweetpea          ║\n'
+        u'│May      |Emerald   |Lily Of The Valley║\n'
+        u'│June     |Pearl     |Rose              ║\n'
+        u'│July     |Ruby      |Larkspur          ║\n'
+        u'│August   |Peridot   |Gladiolus         ║\n'
+        u'│September|Sapphire  |Aster             ║\n'
+        u'│October  |Opal      |Calendula         ║\n'
+        u'│November |Topaz     |Chrysanthemum     ║\n'
+        u'│December |Turquoise |Narcissus         ║\n'
+        u'+=========+==========+==================+'
     )
