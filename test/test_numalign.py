ca52552d1cfe4a7d5cc0ae254f0e564cde013231
John T. Wodder II <git@varonathe.org>
2018-11-06 14:17:07 -0500

    Failing corner cases

--- test/test_numalign.py
+++ test/test_numalign.py
@@ -333,3 +333,119 @@ def test_align_nr_long_header(align):
         '|Glarch|       1.2345|\n'
         '+------+-------------+'
     )
+
+def test_align_n_no_border():
+    tbl = Txtble(
+        headers=['Thing', 'Value'],
+        data=[
+            ['Foo', 12345],
+            ['Bar', 1234.5],
+            ['Baz', 123.45],
+            ['Quux', 12.345],
+            ['Glarch', 1.2345],
+            ['Gnusto', .12345],
+        ],
+        align=['l', 'n'],
+        border=False,
+    )
+    assert str(tbl) == (
+        'Thing |Value\n'
+        '------+-----------\n'
+        'Foo   |12345\n'
+        'Bar   | 1234.5\n'
+        'Baz   |  123.45\n'
+        'Quux  |   12.345\n'
+        'Glarch|    1.2345\n'
+        'Gnusto|    0.12345'
+    )
+
+def test_align_n_header_number():
+    tbl = Txtble(
+        headers=['Thing', 12.345],
+        data=[
+            ['Foo', 12345],
+            ['Bar', 1234.5],
+            ['Baz', 123.45],
+            ['Quux', 12.345],
+            ['Glarch', 1.2345],
+            ['Gnusto', .12345],
+        ],
+        align=['l', 'n'],
+    )
+    assert str(tbl) == (
+        '+------+-----------+\n'
+        '|Thing |   12.345  |\n'
+        '+------+-----------+\n'
+        '|Foo   |12345      |\n'
+        '|Bar   | 1234.5    |\n'
+        '|Baz   |  123.45   |\n'
+        '|Quux  |   12.345  |\n'
+        '|Glarch|    1.2345 |\n'
+        '|Gnusto|    0.12345|\n'
+        '+------+-----------+'
+    )
+
+def test_align_n_long_header_number():
+    tbl = Txtble(
+        headers=['Thing', 123456.654321],
+        data=[
+            ['Foo', 12345],
+            ['Bar', 1234.5],
+            ['Baz', 123.45],
+            ['Quux', 12.345],
+            ['Glarch', 1.2345],
+            ['Gnusto', .12345],
+        ],
+        align=['l', 'n'],
+    )
+    assert str(tbl) == (
+        '+------+-------------+\n'
+        '|Thing |123456.654321|\n'
+        '+------+-------------+\n'
+        '|Foo   | 12345       |\n'
+        '|Bar   |  1234.5     |\n'
+        '|Baz   |   123.45    |\n'
+        '|Quux  |    12.345   |\n'
+        '|Glarch|     1.2345  |\n'
+        '|Gnusto|     0.12345 |\n'
+        '+------+-------------+'
+    )
+
+def test_align_n_numeric_row_fill():
+    tbl = Txtble(
+        [
+            ['Foo', 12345],
+            ['Bar', 123.45, 54.321],
+            ['Baz', 1.2345],
+        ],
+        align='n',
+        row_fill=1.2,
+    )
+    assert str(tbl) == (
+        '+---+----------+------+\n'
+        '|Foo|12345     | 1.2  |\n'
+        '|Bar|  123.45  |54.321|\n'
+        '|Baz|    1.2345| 1.2  |\n'
+        '+---+----------+------+'
+    )
+
+def test_align_n_numeric_header_fill():
+    tbl = Txtble(
+        [
+            ['Foo', 12345, 1.2],
+            ['Bar', 123.45, 54.321],
+            ['Baz', 1.2345, 2.1],
+        ],
+        align='n',
+        header_fill=54321.12345,
+        headers=['Col 1', 'Col 2'],
+    )
+    assert str(tbl) == (
+        '+-----+----------+-----------+\n'
+        '|Col 1|Col 2     |54321.12345|\n'
+        '+-----+----------+-----------+\n'
+        '|Foo  |12345     |    1.2    |\n'
+        '|Bar  |  123.45  |   54.321  |\n'
+        '|Baz  |    1.2345|    2.1    |\n'
+        '+-----+----------+-----------+'
+    )
